18b77607a047e3116d76bb04026e762e6626e892
codeasm <nico.vijlbrief@outlook.com>
2019-12-19 20:34:19 +0100

    module options

--- Kbuild
+++ Kbuild
@@ -1,3 +1,3 @@
-EXTRA_CFLAGS = -Wall -g
+EXTRA_CFLAGS = -Wall -g -std=gnu99
 
 obj-m        = hello.o
--- hello.c
+++ hello.c
@@ -3,13 +3,20 @@
 #include <linux/module.h>
 
 MODULE_DESCRIPTION("My kernel module");
-MODULE_AUTHOR("Me");
+MODULE_AUTHOR("CodeAsm");
 MODULE_LICENSE("GPL");
+
+static char *whom = "world";
+static int howmany = 1;
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
 
 static int dummy_init(void)
 {
        // pr_debug("Hi\n");
-    printk(KERN_ALERT "Hello World!\n");   
+    for( int i = howmany; i>=0;i--)
+    printk(KERN_ALERT "Hello %s!\n", whom);   
+    
     return 0;
 }
 
@@ -17,7 +24,8 @@ static void dummy_exit(void)
 {
 
     //pr_debug("Bye\n");
-    printk(KERN_ALERT "Bye everyone!\n");
+    for( int i = howmany; i>=0;i--)
+    printk(KERN_ALERT "Bye %s!\n", whom);
 }
 
 module_init(dummy_init);
