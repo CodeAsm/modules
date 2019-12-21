7b6d7ecd8defeb5f438cf1f09e52261c0f454f63
codeasm <nico.vijlbrief@outlook.com>
2019-12-21 20:12:03 +0100

    counting works differently

--- hello.c
+++ hello.c
@@ -14,7 +14,7 @@ module_param(whom, charp, S_IRUGO);
 static int dummy_init(void)
 {
        // pr_debug("Hi\n");
-    for( int i = howmany; i>=0;i--)
+    for( int i = howmany; i>0;i--)
     printk(KERN_ALERT "Hello %s!\n", whom);   
     
     return 0;
@@ -24,7 +24,7 @@ static void dummy_exit(void)
 {
 
     //pr_debug("Bye\n");
-    for( int i = howmany; i>=0;i--)
+    for( int i = howmany; i>0;i--)
     printk(KERN_ALERT "Bye %s!\n", whom);
 }
 
